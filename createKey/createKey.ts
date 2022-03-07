e3c5d663748cf1ed1fbd179c07c5bdc83339d7ff
Charles <forson.charlesj@gmail.com>
2022-03-07 17:15:55 +0000

    Refactored preprocessNewPassword

--- createKey/createKey.ts
+++ createKey/createKey.ts
@@ -1,9 +1,11 @@
 export function preprocessNewPassword(password: string) {
+  checkPasswordMeetsRequirements(password);
+}
+
+function checkPasswordMeetsRequirements(password: string) {
   if (password.length < 8) throw new Error(`Password: ${password} too short`);
   const passwordRegex =
     /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[.#?!@$%^&*-]).{8,}$/;
   if (!password.match(passwordRegex))
     throw new Error(`Password: ${password} does not meet RegEx`);
-
-
 }
