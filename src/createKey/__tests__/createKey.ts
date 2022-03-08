d9a8a648a67bea2da3cbb2836a66b9141c50c44c
Charles <forson.charlesj@gmail.com>
2022-03-08 11:04:57 +0000

    Tested secret key gen

--- src/createKey/__tests__/createKey.ts
+++ src/createKey/__tests__/createKey.ts
@@ -1,8 +1,8 @@
-import { createKey } from "../createKey";
+import { createPBKDF2Key } from "../createKey";
 
 test("createKey creates key given password", () => {
   const saltString = "salt";
   const salt = Buffer.from(saltString, "utf-8");
-  const key = createKey("password", salt, 6, 1).toString("hex");
+  const key = createPBKDF2Key("password", salt, 6, 1).toString("hex");
   expect(key).toBe("18");
 });
